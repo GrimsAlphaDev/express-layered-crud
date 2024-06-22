7d37409726d73bf0e6b66283c0042e17f7273e1b
GrimsAlphaDev <kungkingkangcanyouseemekuking@gmail.com>
2024-06-22 22:46:14 +0700

    update api

--- api/index.js
+++ api/index.js
@@ -1,3 +1,28 @@
-import app from './src/index';
+const express = require("express");
+const dotenv = require("dotenv");
 
-export default app;
+const app = express();
+
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+app.get("/api", (req, res) => {
+    res.send("Hello World");
+});
+
+const productController = require('./product/product.controller');
+
+app.use('/product', productController);
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port: ${PORT}`);
+});
+
+module.exports = app;
